d4b7f626945a80379b3e8ad7c6100e99b8994d25
JasmineKaurVirdi <209530796+JasmineKaurVirdi@users.noreply.github.com>
2025-10-05 19:04:55 +0530

    Added dotenv

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-import os
+from dotenv import load_dotenv
+
+load_dotenv() #Load environment variables
 SECRET_KEY=os.environ.get('SECRET_KEY)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
