87826df43424d54c1f961fa29a420d1ff489a7e6
JasmineKaurVirdi <209530796+JasmineKaurVirdi@users.noreply.github.com>
2025-10-06 23:41:27 +0530

    Removed unwanted quote from secret_key

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from dotenv import load_dotenv
 
 load_dotenv() #Load environment variables
-SECRET_KEY=os.environ.get('SECRET_KEY)')
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
